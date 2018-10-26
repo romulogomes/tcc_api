179f17c66f7b67fb5dec380f437e1894598e1281
Romulo <romulogomes13@hotmail.com>
2018-10-26 09:13:10 -0300

    organiza codigo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      # resources :articles 
-      get  'articles' => 'articles#index'
-      post 'articles/save' => 'articles#save'    
-      
-      get    'students' => 'students#mostrar'
-      get    'students/:id' => 'students#find'
-      post   'students' => 'students#save'
+            
+      get    'students'                 => 'students#mostrar'
+      get    'students/:id'             => 'students#find'
+      post   'students'                 => 'students#save'
       post   'students/dados_completos' => 'students#exibe_dados_completos'
-      put    'students'   =>   'students#update' 
-      delete 'students/:id'  => 'students#remove'
+      put    'students'                 => 'students#update' 
+      delete 'students/:id'             => 'students#remove'
 
-      get    'orientadores' => 'advisors#index'
-      get    'orientadores/:id' => 'advisors#find'     
-      post   'orientadores' => 'advisors#save'
-      put    'orientadores' => 'advisors#update'
-      delete 'orientadores/:id'  => 'advisors#remove'
+      get    'orientadores'             => 'advisors#index'
+      get    'orientadores/:id'         => 'advisors#find'     
+      post   'orientadores'             => 'advisors#save'
+      put    'orientadores'             => 'advisors#update'
+      delete 'orientadores/:id'         => 'advisors#remove'
+      
   	end
   end
 end
