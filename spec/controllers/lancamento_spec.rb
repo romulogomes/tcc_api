9fb410089783895e9de37a00cac0b65793c901d7
Romulo <romulogomes13@hotmail.com>
2018-11-08 15:43:19 -0300

    add spec lancamentos

---
+++ spec/controllers/lancamento_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+# require './spec/factories/conta_factory'
+
+RSpec.describe Api::V1::LancamentosController, type: :controller do
+    
+    describe "GET find" do  
+        it "Retornar um Orientador" do
+          
+          # lancamento = FactoryBot.build(:lancamento, conta_credito: conta[:id], );
+          
+          
+          
+          expect(1).to eq 1
+        end
+    end
+end
