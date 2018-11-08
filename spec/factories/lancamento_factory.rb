fdd2bb47b0fbd4d8e924b292438b4f62e9bfdf61
Romulo <romulogomes13@hotmail.com>
2018-11-08 16:51:07 -0300

    add data lancamento

--- spec/factories/lancamento_factory.rb
+++ spec/factories/lancamento_factory.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
     factory :lancamento do
         valor { 10.50 }
         historico { Faker::Lorem.sentence }
+        data { '2018-02-12' }
         # conta_credito { FactoryBot.create(:conta, lancamcodigo: "1"+Faker::Number.decimal(0,2)) }
 
         association :conta_credito, factory: :conta
