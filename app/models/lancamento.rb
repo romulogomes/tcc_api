5cb1ffb5c48640647a9750f92791f6e5c7bda148
Romulo <romulogomes13@hotmail.com>
2018-11-08 16:50:17 -0300

    validate data

--- app/models/lancamento.rb
+++ app/models/lancamento.rb
@@ -1,7 +1,8 @@
 class Lancamento < ApplicationRecord
     validates :conta_credito, presence: true
     validates :conta_debito, presence: true
-	validates :valor, presence: true   
+    validates :valor, presence: true
+    validates :data, presence: true
 
     belongs_to :contas_credito, class_name: "Conta", foreign_key: "conta_credito"
     belongs_to :contas_debito, class_name: "Conta", foreign_key: "conta_debito"
