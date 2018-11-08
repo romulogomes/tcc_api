45646a7a51e51032b9fe52a84119b6b058a09bad
Romulo <romulogomes13@hotmail.com>
2018-11-08 14:43:03 -0300

    add factory

---
+++ spec/factories/conta_factory.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+    factory :conta do
+        natureza 'D'
+        codigo "1.1"
+        descricao { Faker::Company.name }
+        classificacao "Ativo"
+
+        trait :natureza_credora do
+            natureza 'C'
+        end
+    end
+end
--- spec/factories/lancamento_factory.rb
+++ spec/factories/lancamento_factory.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
     factory :lancamento do
-        
+        valor 10.50
+        historico { Faker::Lorem.sentence }
+        association :conta_credito, factory: :conta
     end
 end
