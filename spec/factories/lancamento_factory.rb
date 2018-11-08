47c6f6b8c349a5aa3d07c775aeacf1cddb316242
Romulo <romulogomes13@hotmail.com>
2018-11-08 16:38:08 -0300

    resolve association lancamento e conta

--- spec/factories/lancamento_factory.rb
+++ spec/factories/lancamento_factory.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
     factory :lancamento do
         valor { 10.50 }
         historico { Faker::Lorem.sentence }
+        # conta_credito { FactoryBot.create(:conta, lancamcodigo: "1"+Faker::Number.decimal(0,2)) }
+
         association :conta_credito, factory: :conta
+        association :conta_debito, factory: :conta
     end
 end
