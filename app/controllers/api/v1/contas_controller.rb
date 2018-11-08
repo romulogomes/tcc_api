bde52a7005b938b3a8589731399d1818ea9ae19f
Romulo <romulogomes13@hotmail.com>
2018-11-08 09:49:26 -0300

    crud contas

--- app/controllers/api/v1/contas_controller.rb
+++ app/controllers/api/v1/contas_controller.rb
@@ -7,6 +7,33 @@ module Api
                 # contas = Conta.find(2);
 				render json: contas
             end
+
+            def find
+				conta = Conta.exists?(params[:id]) ? Conta.find(params[:id]) : {};
+				render json: conta
+            end
+            
+            def save
+				conta = Conta.create(contas_params)
+				render json: conta
+            end
+
+            def update
+				conta = Conta.find(contas_params["id"])
+				conta.update(contas_params)
+                render json: conta
+            end
+
+            def remove
+                conta = Conta.find(params[:id]).destroy;
+                render json: conta
+            end
+            
+            private
+			def contas_params
+				params.permit(:id, :natureza, :descricao, :codigo, :classificacao)
+			end
+			
         end
     end
 end
